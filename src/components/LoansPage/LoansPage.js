0f768fdc821b61d9c1127dce402c5e728c61124b
dkag525 <dkag525@gmail.com>
2024-03-15 21:53:08 +0530

    services page

--- src/components/LoansPage/LoansPage.js
+++ src/components/LoansPage/LoansPage.js
@@ -1,7 +1,197 @@
-import React from "react";
+import "../LoansPage/LoansPage.css";
+import Personal_loan_icon from "../../assets/svg/Personal_loan_icon.svg";
+import Corporate_loan_icon from "../../assets/svg/Corporate_loan_icon.svg";
+import Business_loan_icon from "../../assets/svg/Business_loan_icon.svg";
+import Custom_loan_icon from "../../assets/svg/Custom_loan_icon.svg";
 
 const LoansPage = () => {
-  return <div>LoansPage</div>;
+  const Loans = [
+    {
+      img: Personal_loan_icon,
+      LoansName: "Personal Loans",
+      LoansAmount: "$50,000",
+    },
+    {
+      img: Corporate_loan_icon,
+      LoansName: "Corporate Loans",
+      LoansAmount: "$100,000",
+    },
+    {
+      img: Business_loan_icon,
+      LoansName: "Business Loans",
+      LoansAmount: "$500,000",
+    },
+    {
+      img: Custom_loan_icon,
+      LoansName: "Custom Loans",
+      LoansAmount: "Choose Money",
+    },
+  ];
+
+  const LoansData = [
+    {
+      SLNO: "01",
+      LoanMoney: "$100,000",
+      Lefttorepay: "$40,500",
+      Duration: "8 Months",
+      InterestRate: "12%",
+      Installment: "$2,000 / month",
+    },
+    {
+      SLNO: "02",
+      LoanMoney: "$500,000",
+      Lefttorepay: "$250,000",
+      Duration: "36 Months",
+      InterestRate: "10%",
+      Installment: "$8,000 / month",
+    },
+    {
+      SLNO: "03",
+      LoanMoney: "$900,000",
+      Lefttorepay: "$40,500",
+      Duration: "12 Months",
+      InterestRate: "12%",
+      Installment: "$5,000 / month",
+    },
+    {
+      SLNO: "04",
+      LoanMoney: "$50,000",
+      Lefttorepay: "$40,500",
+      Duration: "25 Months",
+      InterestRate: "5%",
+      Installment: "$2,000 / month",
+    },
+    {
+      SLNO: "05",
+      LoanMoney: "$50,000",
+      Lefttorepay: "$40,500",
+      Duration: "5 Months",
+      InterestRate: "16%",
+      Installment: "$10,000 / month",
+    },
+    {
+      SLNO: "06",
+      LoanMoney: "$80,000",
+      Lefttorepay: "$25,500",
+      Duration: "14 Months",
+      InterestRate: "8%",
+      Installment: "$2,000 / month",
+    },
+    {
+      SLNO: "07",
+      LoanMoney: "$12,000",
+      Lefttorepay: "$5,500",
+      Duration: "9 Months",
+      InterestRate: "13%",
+      Installment: "$500 / month",
+    },
+    {
+      SLNO: "08",
+      LoanMoney: "$160,000",
+      Lefttorepay: "$100,800",
+      Duration: "3 Months",
+      InterestRate: "12%",
+      Installment: "$900 / month",
+    },
+  ];
+
+  let TotalLoanMoney = 0;
+  let TotalLefttorepay = 0;
+  let TotalInstallment = 0;
+
+  LoansData.forEach((el) => {
+    TotalLoanMoney =
+      TotalLoanMoney +
+      parseFloat(el.LoanMoney.replace("$", "").replace(",", ""));
+    TotalLefttorepay =
+      TotalLefttorepay +
+      parseFloat(el.Lefttorepay.replace("$", "").replace(",", ""));
+    TotalInstallment =
+      TotalInstallment +
+      parseFloat(
+        el.Installment.replace("$", "")
+          .replace(",", "")
+          .replace("/", "")
+          .replace("month", "")
+      );
+  });
+
+  return (
+    <>
+      <div className="Loans-body-top-container Margin_Bottom_UnderHeadSec">
+        {Loans.map((el, i) => {
+          return (
+            <div key={i} className="TypeOfLoan-body">
+              <img className="Loan_Icon" src={el.img} alt="Loan_Icon" />
+              <div>
+                <p className="font-weight-400-16px secondary-color LineHeight">
+                  {el.LoansName}
+                </p>
+                <p className="font-weight-600-20px">{el.LoansAmount}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="Loans-body-middle-container">
+        <div className="Active_Loans_Overview">
+          <div className="font-weight-600-22px Margin_Bottom_UnderHeadSec">
+            Active Loans Overview
+          </div>
+          <div className="Active_Loans_body">
+            <table className="Loans-Table">
+              <tr>
+                <th className="Loans-Table-Head">SL No</th>
+                <th className="Loans-Table-Head">Loan Money</th>
+                <th className="Loans-Table-Head">Left to repay</th>
+                <th className="Loans-Table-Head">Duration</th>
+                <th className="Loans-Table-Head">Interest Rate</th>
+                <th className="Loans-Table-Head">Installment</th>
+                <th className="Loans-Table-Head">Repay</th>
+              </tr>
+              <div className="TableGap"></div>
+
+              {LoansData.map((el, i) => {
+                return (
+                  <>
+                    <tr key={i}>
+                      <td className="Loans-Table-Data">{el.SLNO}</td>
+                      <td className="Loans-Table-Data">{el.LoanMoney}</td>
+                      <td className="Loans-Table-Data">{el.Lefttorepay}</td>
+                      <td className="Loans-Table-Data">{el.Duration}</td>
+                      <td className="Loans-Table-Data">{el.InterestRate}</td>
+                      <td className="Loans-Table-Data">{el.Installment}</td>
+                      <td className="Loans-Table-Data">
+                        <div className="LoansButton font-weight-500-15px">
+                          Repay
+                        </div>
+                      </td>
+                    </tr>
+                    <div className="TableGap"></div>
+                  </>
+                );
+              })}
+
+              <tr>
+                <td className="LoansTotalData">Total</td>
+                <td className="LoansTotalData">
+                  ${TotalLoanMoney.toLocaleString()}
+                </td>
+                <td className="LoansTotalData">
+                  ${TotalLefttorepay.toLocaleString()}
+                </td>
+                <td className="LoansTotalData"></td>
+                <td className="LoansTotalData"></td>
+                <td className="LoansTotalData">
+                  ${TotalInstallment.toLocaleString()}/ month
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoansPage;
--- src/components/ServicesPage/ServicesPage.js
+++ src/components/ServicesPage/ServicesPage.js
@@ -1,7 +1,212 @@
 import React from "react";
+import "../ServicesPage/ServicesPage.css";
+import LifeIns_icon from "../../assets/svg/LifeIns_icon.svg";
+import Shopping_icon from "../../assets/svg/Shopping_icon.svg";
+import Safety_icon from "../../assets/svg/Safety_icon.svg";
+import Business_loan_icon from "../../assets/svg/Business_icon.svg";
+import CheckingAccount_icon from "../../assets/svg/CheckingAccount_icon.svg";
+import Saving_accounts_icon from "../../assets/svg/Saving_accounts_icon.svg";
+import DabitAndCredit_icon from "../../assets/svg/DabitAndCredit_icon.svg";
+import LifeInsurance_icon from "../../assets/svg/LifeInsurance_icon.svg";
 
 const ServicesPage = () => {
-  return <div>ServicesPage</div>;
+  const Services = [
+    {
+      img: LifeIns_icon,
+      ServiceType: "Life Insurence",
+      ServicesOption: "Unlimited protection",
+    },
+    {
+      img: Shopping_icon,
+      ServiceType: "Shopping",
+      ServicesOption: "Buy. Think. Grow",
+    },
+    {
+      img: Safety_icon,
+      ServiceType: "Safety",
+      ServicesOption: "we are your allies",
+    },
+  ];
+
+  const BankServicesList = [
+    {
+      img: Business_loan_icon,
+      ServicesType: "Business loans",
+      ServicesOption: "it is long established",
+      ServicesName1: "Lorem Ipsum",
+      PublishStatus1: "Many Publishing",
+      ServicesName2: "Lorem Ipsum",
+      PublishStatus2: "Many Publishing",
+      ServicesName3: "Lorem Ipsum",
+      PublishStatus3: "Many Publishing",
+    },
+    {
+      img: CheckingAccount_icon,
+      ServicesType: "Business loans",
+      ServicesOption: "it is long established",
+      ServicesName1: "Lorem Ipsum",
+      PublishStatus1: "Many Publishing",
+      ServicesName2: "Lorem Ipsum",
+      PublishStatus2: "Many Publishing",
+      ServicesName3: "Lorem Ipsum",
+      PublishStatus3: "Many Publishing",
+    },
+    {
+      img: Saving_accounts_icon,
+      ServicesType: "Business loans",
+      ServicesOption: "it is long established",
+      ServicesName1: "Lorem Ipsum",
+      PublishStatus1: "Many Publishing",
+      ServicesName2: "Lorem Ipsum",
+      PublishStatus2: "Many Publishing",
+      ServicesName3: "Lorem Ipsum",
+      PublishStatus3: "Many Publishing",
+    },
+    {
+      img: DabitAndCredit_icon,
+      ServicesType: "Business loans",
+      ServicesOption: "it is long established",
+      ServicesName1: "Lorem Ipsum",
+      PublishStatus1: "Many Publishing",
+      ServicesName2: "Lorem Ipsum",
+      PublishStatus2: "Many Publishing",
+      ServicesName3: "Lorem Ipsum",
+      PublishStatus3: "Many Publishing",
+    },
+    {
+      img: LifeInsurance_icon,
+      ServicesType: "Business loans",
+      ServicesOption: "it is long established",
+      ServicesName1: "Lorem Ipsum",
+      PublishStatus1: "Many Publishing",
+      ServicesName2: "Lorem Ipsum",
+      PublishStatus2: "Many Publishing",
+      ServicesName3: "Lorem Ipsum",
+      PublishStatus3: "Many Publishing",
+    },
+    {
+      img: Business_loan_icon,
+      ServicesType: "Business loans",
+      ServicesOption: "it is long established",
+      ServicesName1: "Lorem Ipsum",
+      PublishStatus1: "Many Publishing",
+      ServicesName2: "Lorem Ipsum",
+      PublishStatus2: "Many Publishing",
+      ServicesName3: "Lorem Ipsum",
+      PublishStatus3: "Many Publishing",
+    },
+  ];
+
+  return (
+    <>
+      <div className="Services-Page-Top-Container Margin_Bottom_UnderHeadSec">
+        {Services.map((el, i) => {
+          return (
+            <div key={i} className="ServicesType-Container">
+              <img
+                style={{ marginRight: "1.375rem" }}
+                src={el.img}
+                alt="Services_Icons"
+              />
+              <div className="ServicesName-Body">
+                <p className="font-weight-600-20px">{el.ServiceType}</p>
+                <p className="font-weight-400-16px">{el.ServicesOption}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="Services-Page-Middle-Container">
+        <div className="Services-Page-Container">
+          <div className="font-weight-600-22px Margin_Bottom_UnderHeadSec">
+            Bank Services List
+          </div>
+
+          {BankServicesList.map((el, i) => {
+            const BackGroundColor = () => {
+              let bgcolor;
+              if (i === 0) {
+                bgcolor = "#FFE0EB";
+              } else if (i === 1) {
+                bgcolor = "#FFF5D9";
+              }
+            };
+
+            return (
+              <div key={i} className="bank-services-list-container">
+                <table className="Bank-Services-Table">
+                  <tr>
+                    <th className="Bank-Services-Table-Heading">
+                      <div className="Bank-Services-Name-container">
+                        <div
+                          style={{
+                            background: "pink",
+                            marginRight: "1.375rem",
+                            borderRadius: "1.25rem",
+                            display: "flex",
+                            alignItems: "center",
+                            width: "3.75rem",
+                            height: "3.75rem",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <img src={el.img} alt="icon" />
+                        </div>
+
+                        <div className="BankServicesType-Body">
+                          <p className="head-section-transaction-500-16px BlackColor">
+                            {el.ServicesType}
+                          </p>
+                          <p className="font-weight-400-15px secondary-color">
+                            {el.ServicesOption}
+                          </p>
+                        </div>
+                      </div>
+                    </th>
+                    <th className="Bank-Services-Table-Heading">
+                      <div className="BankServicesType-Body">
+                        <p className="head-section-transaction-500-16px BlackColor">
+                          {el.ServicesName1}
+                        </p>
+                        <p className="font-weight-400-15px secondary-color">
+                          {el.PublishStatus1}
+                        </p>
+                      </div>
+                    </th>
+                    <th className="Bank-Services-Table-Heading">
+                      <div className="BankServicesType-Body">
+                        <p className="head-section-transaction-500-16px BlackColor">
+                          {el.ServicesName2}
+                        </p>
+                        <p className="font-weight-400-15px secondary-color">
+                          {el.PublishStatus2}
+                        </p>
+                      </div>
+                    </th>
+                    <th className="Bank-Services-Table-Heading">
+                      <div className="BankServicesType-Body">
+                        <p className="head-section-transaction-500-16px BlackColor">
+                          {el.ServicesName3}
+                        </p>
+                        <p className="font-weight-400-15px secondary-color">
+                          {el.PublishStatus3}
+                        </p>
+                      </div>
+                    </th>
+                    <th className="Bank-Services-Table-Heading">
+                      <div className="Services-Btn font-weight-500-15px">
+                        View Details
+                      </div>
+                    </th>
+                  </tr>
+                </table>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ServicesPage;
